2b1b526a6cf35d3be1590a36c9789de03d1675fe
UjjwalKumar7209 <ujjwal720946@gmail.com>
2026-02-13 03:24:24 +0530

    contact and menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import gsap from "gsap";
-import { ScrollTrigger, SplitText } from "gsap/all";
+import { ScrollSmoother, ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
 import Art from "./components/Art";
+import Menue from "./components/Menue";
+import Contact from "./components/Contact";
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
+// gsap.registerPlugin(ScrollSmoother) 
+// ScrollSmoother.create({
+//   smooth: 1,
+//   effects: true
+// });
 const App = () => {
   return (
     <main>
@@ -16,6 +23,8 @@ const App = () => {
         <Cocktails />
         <About />
         <Art />
+        <Menue />
+        <Contact />
     </main>
   );
 }
